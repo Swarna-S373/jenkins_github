0da070f2afeaedbefe6f550861dcb7581cb9bab1
Swarna-S373 <swarnalakshmi10e@gmail.com>
2025-08-26 21:51:22 +0530

    Add files via upload

---
+++ 7thjuly.py
@@ -0,0 +1,41 @@
+print("hello",end=" ")
+#this is editor
+
+#single file / coll of files - module - library
+#running a module - running a file
+print("Hi","I","j") #printed with space
+print("i","iii","j",sep="&") #printed with ,
+print("josh")
+print("23,New Street")
+print("Bangalore")
+print("12345")
+
+print("s") #python 3
+#print "s" #python 2
+
+a=4
+if a<5:
+    print(a)
+    print("Inside the block")
+print("Out of block")
+
+print("Hi")
+
+
+#comments
+'''jgfdsdfghjkl
+
+jhgfder'''
+
+"""iuytsdgm
+h
+
+iuhgfc"""
+
+
+print("Hi")
+
+a=10
+b=20 
+#type - int,float,bool,str
+print(type(a))
